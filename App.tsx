98dd8ed9835f24c7655b29cfb7ae22d973fa17b7
Thiago <thiagocontellid@gmail.com>
2022-09-21 11:19:10 -0300

    feat: add theme type and theme provider

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import {
 	useFonts,
 	Nunito_400Regular,
 	Nunito_700Bold,
 } from '@expo-google-fonts/nunito';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/theme';
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
@@ -13,18 +15,11 @@ export default function App() {
 	});
 
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
-		</View>
+		<ThemeProvider theme={theme}>
+			<View>
+				<Text>Open up App.tsx to start working on your app!</Text>
+				<StatusBar style="auto" />
+			</View>
+		</ThemeProvider>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
