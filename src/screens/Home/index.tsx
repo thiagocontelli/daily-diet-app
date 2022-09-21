64c2a7db688c8644538dd316c3a6800185f091ed
Thiago <thiagocontellid@gmail.com>
2022-09-21 11:49:10 -0300

    feat: Header add to Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
 import { Text } from 'react-native';
+import { Header } from '../../components/Header';
 
 import { Container } from './style';
 
 export function Home() {
 	return (
 		<Container>
+			<Header />
 			<Text>Página Inicial</Text>
 		</Container>
 	);
