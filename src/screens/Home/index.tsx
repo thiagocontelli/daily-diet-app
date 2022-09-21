e0ff9dd25a9c13e91a2e9c567c912b32488ae587
Thiago <thiagocontellid@gmail.com>
2022-09-21 11:21:34 -0300

    feat: home screen created

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+
+import { Container } from './style';
+
+export function Home() {
+	return (
+		<Container>
+			<Text>Página Inicial</Text>
+		</Container>
+	);
+}
