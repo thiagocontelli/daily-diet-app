d98736f0b7d5f7de93d30480577c245a55c4976e
Thiago <thiagocontellid@gmail.com>
2022-09-21 11:59:34 -0300

    feat: userImg and type to jpg

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,9 @@
+import { Image } from 'react-native';
+
 import { Container, Logo, UserImg } from './style';
 
 import logoImg from '../../assets/logo.png';
+import userImg from '../../assets/user.jpg';
 
 export function Header() {
 	return (
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,7 +1,22 @@
+import userImg from '../../assets/user.jpg';
+
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-between;
+`;
 
 export const Logo = styled.Image``;
 
-export const UserImg = styled.View``;
+export const UserImg = styled.View`
+	width: 40px;
+	height: 40px;
+
+	border-radius: 100px;
+
+	background-color: transparent;
+	border: 3px solid black;
+`;
